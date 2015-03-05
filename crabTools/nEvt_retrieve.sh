08be164619cbad6d9f0960459195a667439c3696
afiqaize <afiqaize@gmail.com>
2015-03-05 15:21:24 +0800

    New retrieve script

--- crabTools/nEvt_retrieve.sh
+++ crabTools/nEvt_retrieve.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # A simple script to check the event number in all multicrab jobs agree
+# Usage: . nEvt_retrieve.sh procName logLabel pathName
+# No underscores in the args (except pathName)
 
 # Main directory (the one fed into the multicrab -c)
-mainDir="/afs/cern.ch/work/a/afiqaize/hlt/Part2_OpenHLT/OpenHLT/CMSSW_7_2_1_patch2/src/HLTrigger/Configuration/test/Alt_Job/bar_only/"
-
-# Common part of the name of individual multicrab directories; the differentiator should be at the end
-jobName="dyll_"
-
-# List of the differentiators
-jobList="all
-bar"
+mainDir="/afs/cern.ch/work/a/afiqaize/hlt/Part2_OpenHLT/OpenHLT/CMSSW_7_4_0_pre8/src/drawHist/drawVar/test/crab/"
 
 # The CRAB log file to be searched and other shit
 crabLog="CMSSW_"
 logExt='.log'
 
-outDir=$mainDir"barOnly_log/dyll/"
+# Common part of the name of individual multicrab directories; the differentiator should be at the end
+jobName=$1'_'
 
-readLabel="_seed25"
+# List of the differentiators
+jobList="pu20
+pu40"
+
+outDir=$mainDir"../PUeWP_log/"$1'/'
 
 #Define the directories and search the nEvt (for HLT log only atm) [match the desired pattern based on your job]
 for eachJob in $jobList
@@ -26,14 +26,14 @@ do
 inDir=$mainDir$jobName$eachJob"/res/"
 jobLabel=$jobName$eachJob
 
-grep "0 EB_Seed25" $inDir$crabLog"1.stdout" >& $outDir$jobLabel$readLabel$logExt
+grep "0 $3" $inDir$crabLog"1.stdout" >& $outDir$eachJob'_'$2$logExt
 
 # This needs to be changed to match no. of jobs
 # It starts at 2 since the first and creation is done above
 for i in {2..500}
 do
 
-grep "0 EB_Seed25" $inDir$crabLog$i".stdout" >> $outDir$jobLabel$readLabel$logExt
+grep "0 $3" $inDir$crabLog$i".stdout" >> $outDir$eachJob'_'$2$logExt
 
 done
 done
