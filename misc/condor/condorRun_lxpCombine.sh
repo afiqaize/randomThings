6d3473d5df5417db10e68b4d73c7cfd751651b3e
Afiq Anuar <afiqaize@gmail.com>
2023-02-21 19:43:28 +0100

    actually do combine stuff

--- misc/condor/condorRun_lxpCombine.sh
+++ misc/condor/condorRun_lxpCombine.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+# adapted from condorRun.sh by joseph dulemba @ rochester
 
-set -o noglob
+ulimit -s unlimited
+set -e
+cd ${cmssw_base}/src
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+cd $TMPDIR
 
-### not meant to be run directly; simply a wrapper executable
-export LD_LIBRARY_PATH=${LD_LIB_PATH}
-
+#### not meant to be run directly; simply a wrapper executable
 echo 'Job execution starts at '$(date)' on host '${HOSTNAME}
 echo
 
